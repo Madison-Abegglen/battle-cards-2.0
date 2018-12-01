31fa3925af974422840984796cbdb8fd666b63ba
Madison Abegglen <madison.abegglen@gmail.com>
2018-11-30 18:21:08 -0700

    woo router works, & some css

---
+++ src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <h1 class="game-title">B C | Zombies Edition</h1>
+    <h2 class="game-title">Start Game</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "main",
+  components: {}
+};
+</script>
